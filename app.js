835fa1533568374edcbee9cc814b0bc1b8184300
laxminarayan-dev <jaiswallucky0112@gmail.com>
2026-02-15 18:16:55 +0530

    Cors Update

--- app.js
+++ app.js
@@ -155,7 +155,11 @@ const tablesData = {
         },
     ],
 }
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json())
 app.use("/uploads", express.static("uploads"));
 
