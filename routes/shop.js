3133e69e0b9206cc1b67e876991fbf4100d8c22d
laxminarayan-dev <jaiswallucky0112@gmail.com>
2026-04-11 20:23:49 +0530

    Shop added new socket event fire

--- routes/shop.js
+++ routes/shop.js
@@ -41,7 +41,7 @@ router.post("/add", async (req, res) => {
         }
         const io = getIO();
         io.emit("shop-updated");
-        io.emit("shop-added")
+        io.emit("shop-added", saved)
         res.status(201).json({ shop: saved });
     } catch (error) {
         console.error("Error adding shop:", error);
